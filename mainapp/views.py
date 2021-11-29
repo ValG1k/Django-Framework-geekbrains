b51c0e116b197ba0b4bfc39466216c6a4fdfccc3
ValG1k <valg9909@gmail.com>
2021-11-29 19:46:47 +0700

    less3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,36 +1,59 @@
 from django.shortcuts import render
-
+from django.templatetags.static import static
+import os
+import json
+from mainapp.models import Products, ProductCategory
 # Create your views here.
 
-def index(request):
-    menu = {'title': 'Магазин'}
-    return render(request,"mainapp/index.html", menu)
+module_dir = os.path.dirname(__file__,)
 
-def products(request):
-    return render(request, "mainapp/products.html")
+links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'},
+    ]
+
+menu = [
+        {'href': 'index', 'name': 'главная'},
+        {'href': 'products:index', 'name': 'продукты'},
+        {'href': 'contact', 'name': 'контакты'},
+]
+
+def index(request):
+    context = {'title':'Магазин', 'menu':menu}
+    return render(request, "mainapp/index.html", context)
+
+def products(request, pk=None):
+    print(pk)
+    file_path = os.path.join(module_dir, 'fixtures/products.json')
+    products = json.load(open(file_path, encoding='utf-8'))
+
+
+
+    title = 'Продукты'
+    product = Products.objects.all()[:4]
+    content = {'title': title,
+        'links_menu': links_menu,
+        'products': product,
+        'menu': menu,
+    }
+    # content = {'title': title, 'products': product}
+
+
+    return render(request, 'mainapp/products.html', content)
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    context = {'title': 'Контакты', 'menu':menu}
+    return render(request, "mainapp/contact.html", context)
 
-def context(request):
-    context = {
-        'title':'test context',
-        'header':'Добро подаловать на сайт',
-        'username':'Джон',
-        'products': [
-            {'name':'Стулья','price':6789},
-            {'name':'Диваны','price':12389},
-            {'name':'Столы','price':16789},
-        ]
-    }
-
-    return render(request, "mainapp/test_context.html", context)
-
-def menu(request):
-    links_menu = [
-        {'href':'products_all', 'name':'все'},
-        {'href':'products_home', 'name':'дом'},
-        {'href':'products_office', 'name':'офис'},
-        {'href':'products_classic', 'name':'классика'},
-    ]
-    return render(request,'inc_categories_menu.html', links_menu)
+def main(request):
+     title = 'главная'
+     product = Products.objects.all()[:4]
+     content = {'title': title,
+                'links_menu': links_menu,
+                'products': product,
+                'menu': menu,
+                }
+     return render(request, 'mainapp/products.html', content)
